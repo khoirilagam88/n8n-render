0ec3e84f94e5c205459cdedf66078dd3516dc6a1
khoirilagam88 <khoirilagam88@gmail.com>
2025-10-15 09:59:45 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,21 +1,3 @@
 #!/bin/sh
-# Backup otomatis database & config sebelum n8n start
-
-# Waktu backup
-TIMESTAMP=$(date +%Y%m%d%H%M%S)
-
-# Folder backup
-BACKUP_DIR="/home/node/.n8n_backup"
-mkdir -p $BACKUP_DIR
-
-# Backup database jika ada
-if [ -f "/home/node/.n8n/database.sqlite" ]; then
-    cp /home/node/.n8n/database.sqlite $BACKUP_DIR/database_$TIMESTAMP.sqlite
-fi
-
-# Backup config jika ada
-if [ -f "/home/node/.n8n/config" ]; then
-    cp /home/node/.n8n/config $BACKUP_DIR/config_$TIMESTAMP
-fi
-
-echo "Backup selesai: $TIMESTAMP"
+mkdir -p /home/node/.backup_conflicts
+cp -r /home/node/.n8n/* /home/node/.backup_conflicts/
