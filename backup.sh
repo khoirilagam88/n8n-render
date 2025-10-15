60ae229238f653adde731ae307019306718161a3
khoirilagam88 <khoirilagam88@gmail.com>
2025-10-15 10:16:23 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Backup workflows
+# Backup workflows ke folder .n8n_backup
 mkdir -p /home/node/.n8n_backup
 n8n export:all --output /home/node/.n8n_backup/workflows.json || echo "No workflows to backup"
 
